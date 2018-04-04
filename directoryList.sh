0911471badc1d99f9bce9910de30bb18aaaffd29
David Pham <dav.h.pham@gmail.com>
2018-04-04 04:47:53 +0000

    made changes from echo to printf

--- directoryList.sh
+++ directoryList.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#bash script to echo all files in a directory
+#bash script to print all files in a directory
 
 #variables
 
-echo "Here are all the files in your directory"
+printf "Here are all the files in your directory\n" 
 ls
-echo "Here are all your files and permissions in your directory"
+printf "Here are all your files and permissions in your directory\n"
 ls -all
 echo "Exiting program"
 exit
