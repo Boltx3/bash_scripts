ce86474f8b6a3890f0ac19f780098ba16253571b
David Pham <dav.h.pham@gmail.com>
2018-04-04 03:32:36 +0000

    First commit

---
+++ fileName.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#bash script to echo all files in a directory
+
+#variables
+
+echo "Here are all the files in your directory"
+ls
+echo "Here are all your files and permissions in your directory"
+ls -all
+echo "Exiting program"
+exit
